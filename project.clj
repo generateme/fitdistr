5945182b70ee7e6977e4bffd2c2b4cb83608378b
generateme <generateme.blog@gmail.com>
2019-07-17 17:39:26 +0200

    scm added

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject generateme/fitdistr "0.0.1-SNAPSHOT"
   :description "Distribution fitting with MLE, MGE and QME methods."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/generateme/fitdistr"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :scm {:name "git"
+        :url "https://github.com/generateme/fitdistr/"}  
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [generateme/fastmath "1.4.0-SNAPSHOT"]])
